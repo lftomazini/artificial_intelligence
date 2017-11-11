d503b7031ceaaf6597011d5520525f755b88a6cc
lftomazini <lftomazini@hotmail.com>
2017-11-11 18:08:19 -0200

    Refactored

--- sudoku.py
+++ sudoku.py
@@ -17,46 +17,48 @@ def print_sudoku(board):
 			print("|" + "---+"*8 + "---|")
 		else:
 			print("|" + "   +"*8 + "   |")
-	time.sleep(3)
+	time.sleep(1)
 
-# def solve(board):
-# 	for row in board:
-# 		options = np.setdiff1d(possibilities, row)
-# 		print(row)
-# 		print(options)
-# 		for column in range(board):
-# 			options = np.setdiff1d(options, board[:column])
-# 			# print(column)
-# 			print(options)
-# 		options  = np.array([])
+
+def assigned_in_row(board, row, option):
+	row_options = np.setdiff1d(possibilities, board[row])
+	print(row_options)
+	if (option in row_options):
+		print(str(option) + " was already used in row " + str(row))
+		return True
+	else:
+		print(str(option) + " was NOT used in row " + str(row))
+		return False
+
+def assigned_in_column(board, column, option):
+	column_options = np.setdiff1d(possibilities, board[:, column])
+	print(column_options)
+	if (option in column_options):
+		print(str(option) + " was already used in column " + str(column))
+		return True
+	else:
+		print(str(option) + " was NOT used in column " + str(column))
+		return False
+
+def assigned_in_grid(board, row, column, option):
+	return True
+
+def safe_position(board, row, column, option):
+	return assigned_in_row(board, row, option) and assigned_in_column(board, column, option) and assigned_in_grid(board, row, column, option)
 
 def solve(board):
 	dimension = len(board[0])
 
 	for row in range(dimension):
-		row_options = np.setdiff1d(possibilities, board[row])
-		# print(board[row])
-		# print(row_options)
 		for column in range(dimension):
-			column_options = np.setdiff1d(possibilities, board[:, column])
-			options = np.intersect1d(row_options, column_options)
-			# print(board[:, column])
-			# print(column_options)
-			# print(options)
-
-			if (board[row ,column] == 0):
-				board[row, column] = options[0]
-				print_sudoku(board)
-
-
-
-
-			options = np.array([])
-			# print(options)
-			# print('----------------')
+			for option in range (1, 10):
+				if (board[row ,column] == 0):
+					if (safe_position(board, row, column, option)):
+						board[row, column] = option
+						print_sudoku(board)
 
 
 if __name__ == "__main__":
-   grid = np.loadtxt(sys.argv[1]).astype(int)
-   print_sudoku(grid)
-   solve(grid)
+   board = np.loadtxt(sys.argv[1]).astype(int)
+   print_sudoku(board)
+   solve(board)
