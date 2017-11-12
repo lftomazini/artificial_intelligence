01e3675dbc550cd511f5cb6bd3454c86d8af1cd5
lftomazini <lftomazini@hotmail.com>
2017-11-12 00:10:33 -0200

    Uninformed search comlete

--- sudoku.py
+++ sudoku.py
@@ -4,8 +4,15 @@ import numpy as np
 import math
 import time
 
+# Possible numbers to put
 possibilities = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
+# Used to backtrack, appends a tuble containing the coordinates and the number put
+filled = []
+
+'''
+Prints the sudoku board
+'''
 def print_sudoku(board):
 	os.system('clear')
 	print("-"*37)
@@ -17,59 +24,77 @@ def print_sudoku(board):
 			print("|" + "---+"*8 + "---|")
 		else:
 			print("|" + "   +"*8 + "   |")
-	time.sleep(1)
+	# time.sleep(1)
 
 
+# Verifies if a given number was put in a board row
 def assigned_in_row(board, row, option):
 	row_options = np.setdiff1d(possibilities, board[row])
-	# print(row_options)
 	if (option in row_options):
-		# print(str(option) + " was already used in row " + str(row))
 		return True
 	else:
-		# print(str(option) + " was NOT used in row " + str(row))
 		return False
 
+
+# Verifies if a given number was put in a board column
 def assigned_in_column(board, column, option):
 	column_options = np.setdiff1d(possibilities, board[:, column])
-	# print(column_options)
 	if (option in column_options):
-		# print(str(option) + " was already used in column " + str(column))
 		return True
 	else:
-		# print(str(option) + " was NOT used in column " + str(column))
 		return False
 
+
+# Verifies if a given number was put in a board grid
 def assigned_in_grid(board, row, column, option):
 	dimension = len(board[0])
 	row = row // 3 * 3
 	column = column // 3 * 3
 
 	grid = board[row: row + 3, column: column + 3]
-	# print(grid)
 	grid = np.reshape(grid, dimension)
 	grid_options = np.setdiff1d(possibilities, grid)
-	# print(grid_options)
 
 	if (option in grid_options):
 		return True
 	else:
 		return False
 
+
+# Verifies if a number wasn't put in neither a row, column or grid
 def safe_position(board, row, column, option):
 	return assigned_in_row(board, row, option) and assigned_in_column(board, column, option) and assigned_in_grid(board, row, column, option)
 
+
+# Solves the sudoku board, backtracking when it gets stuck
 def solve(board):
 	dimension = len(board[0])
+	row = 0
+	column = 0
+	option = 1
 
-	for row in range(dimension):
-		for column in range(dimension):
-			for option in range (1, 10):
-				if (board[row ,column] == 0):
+	while (row < dimension):
+		while (column < dimension):
+			if (board[row ,column] == 0):
+				while (option <=  10):
 					if (safe_position(board, row, column, option)):
 						board[row, column] = option
+						filled.append([row, column, option])
 						print_sudoku(board)
-
+						break
+					option += 1
+				if (option == 11):
+					row = filled[-1][0]
+					column = filled[-1][1] - 1
+					option = filled[-1][2] + 1
+					board[row, column + 1] = 0
+					print_sudoku(board)
+					del filled[-1]
+				else:
+					option = 1
+			column += 1
+		column = 0
+		row += 1
 
 if __name__ == "__main__":
    board = np.loadtxt(sys.argv[1]).astype(int)
