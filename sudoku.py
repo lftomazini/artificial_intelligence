2c0863fa8aab2cd19b54a3d06d6a5f468ec1d44b
Romão Martines <roh.martines@gmail.com>
2017-11-17 16:03:49 -0200

    New inplementation of guided search.

--- sudoku.py
+++ sudoku.py
@@ -10,9 +10,6 @@ possibilities = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
 # Used to backtrack, appends a tuple containing the coordinates and the number put
 filled = []
 
-# Used to bactrack in a guided search
-heuristic_array = []
-
 # Prints the sudoku board
 def print_sudoku(board):
     os.system('clear')
@@ -103,56 +100,47 @@ A-Star algorithm to solve Sudoku
 Reference: https://github.com/SAURABHMARATHE/a-star_algorithm_code_for_sudoku/blob/master/my_a_star_for_sudoku.py
 '''
 
+# Calculate the possible values for each empty cell on board
+def possible_values(board, row, column):
+    dimension = len(board)
+
+    if (board[row, column] != 0):
+        return -1
+
+    else:
+        possible_values = []
+        for option in range(1, 10):
+            if (safe_position(board, row, column, option)):
+                possible_values.append(option)
+        return len(possible_values)
+
 # Calculate the heuristic associated with each cell
 def heuristic (board):
-    # Array to store the possible values for each cell
-
-
+    dimension = len(board[0])
     bestRow = 0
     bestColumn = 0
-    dimension = len(board[0])
+    heur = -1
 
     for row in range (0, dimension):
         for column in range (0, dimension):
             if (board[row, column] == 0):
-                possible_values = []
-                for option in range(1, 10):
-                    if (safe_position(board, row, column, option)):
-                        possible_values.append(option)
-
-                # Heuristic is the length of possible_values. The shorter is the array, the better is the heuristic.
-                heuristic_array.append([row, column, len(possible_values)])
-
-    # Calculate the cost of heuristic and takes the best position for the board.
-    dimension  = len(possible_values)
-    bestRow = heuristic_array[row][0]
-    bestColumn = heuristic_array[row][1]
-    minimum = heuristic_array[0][2]
-
-    # Search for the minimum heuristic (best first)
-    for row in range(0, dimension):
-        if(heuristic_array[row][2] < minimum):
-            bestRow = heuristic_array[row][0]
-            bestColumn = heuristic_array[row][1]
-            minimum = heuristic_array[row][2]
-
-    del heuristic_array[bestRow]
+                if ((heur == -1) and (heur > possible_values(board, row, column))):
+                    bestRow = row
+                    bestColumn = column
 
     return bestRow, bestColumn
 
-# Solve the Sudoku board using the A Star Algorithm
-def run_a_star (board):
+def solve_a_star(board):
     solved_cells = np.count_nonzero(board)
-    # print(solved_cells)
 
     while (solved_cells < 81):
         row, column = heuristic(board)
-        for option in range (1, 10):
+        for option in range(1, 10):
             if (safe_position(board, row, column, option)):
                 board[row, column] = option
                 filled.append([row, column, option])
                 print_sudoku(board)
-                solved_cells += 1
+                solved_cells = np.count_nonzero(board)
                 break
 
 if __name__ == "__main__":
@@ -160,7 +148,7 @@ if __name__ == "__main__":
 
     if (str(sys.argv[1]) == "-i"):
         print_sudoku(board)
-        run_a_star(board)
+        solve_a_star(board)
     elif (str(sys.argv[1]) == "-u"):
         print_sudoku(board)
         solve(board)
