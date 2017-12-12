76f81e54d61b526d6948f45c2a0f34e158ecffd0
lftomazini <lftomazini@hotmail.com>
2017-12-12 11:14:48 -0200

    Comments

--- k-means/clustering.py
+++ k-means/clustering.py
@@ -3,12 +3,14 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 import pylab as pl
+import itertools
 
 data = np.array([])
 centroids = np.array([])
 groups = [0]
 new_groups = [1]
 color_dict = {0: 'red', 1: 'black'}
+labels = ['Sample code number', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape', 'Marginal Adhesion', 'Single Epithelial Cell Size', 'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
 
 np.set_printoptions(threshold='nan')
 np.set_printoptions(suppress=True)
@@ -16,11 +18,14 @@ np.set_printoptions(suppress=True)
 ''' Puts average of column for nan and scales values in range 0 to 1'''
 def preprocessing():
 	global data
+	global max_value
+
 	col_mean = np.nanmean(data, axis=0)
 	indexes = np.where(np.isnan(data))
 	data[indexes] = np.take(col_mean, indexes[1])
 	data = data / data.max(axis=0)
 
+''' Create the clusters based on the closest neighbour'''
 def clusters():
 	global data
 	global centroids
@@ -37,13 +42,7 @@ def clusters():
 		new_groups.append(distances.index(min(distances)))
 	redefine_centroids()
 
-def plot_data():
-	global data
-
-	fig, ax = plt.subplots()
-	ax.scatter(np.array(data[:,4]),np.array(data[:,6]), c=map(color_dict.get, np.floor(data[:,-1])), edgecolors = map(color_dict.get, groups), linewidth = 1.5)
-	plt.show()
-
+''' Redefines the centroids based on the new clusters generated'''
 def redefine_centroids():
 	global data
 	global centroids
@@ -59,13 +58,34 @@ def redefine_centroids():
 	centroids[0] = np.mean(c1, axis=0)
 	centroids[1] = np.mean(c2, axis=0)
 
+''' Plots a bidimensional graph of the two attributes'''
+def plot_data(x_attribute, y_attribute):
+	global data
+
+	fig, ax = plt.subplots()
+	ax.set_xlabel(labels[x_attribute])
+	ax.set_ylabel(labels[y_attribute])
+	ax.scatter(np.array(data[:,x_attribute]),np.array(data[:,y_attribute]), c=map(color_dict.get, np.floor(data[:,-1])), edgecolors = map(color_dict.get, groups), linewidth = 1.5)
+	# plt.show()
+	plt.savefig('att' + str(x_attribute) + '_x_att' + str(y_attribute))
+
+''' Generates al graphs up to the nth attribute'''
+def generate_figures(num_attributes):
+	attributes = itertools.combinations(range(1, num_attributes + 1), 2)
+	for item in attributes:
+		plot_data(item[0], item[1])
+
 
 if __name__ == "__main__":
 	data = np.genfromtxt(sys.argv[1])
 	preprocessing()
 
+	''' Assign random initial position for the centroids'''
 	centroids = np.random.rand(2, 11)
 
+	''' Repeats the process while there is no convergence'''
 	while (groups != new_groups):
 		clusters()
-	plot_data()
+
+	''' Exports results'''
+	generate_figures(5)
